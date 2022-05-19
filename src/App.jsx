84bc2a1d21f37be815fb64bf21c18406ba37a513
Ademola Adebowale <ademolajohn844@gmail.com>
2022-05-19 09:51:17 +0100

    dashboard update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import Dashboard from './pages/dashboard/Dashboard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      {/* BrowserRouter */}
+
       <Dashboard />
     </div>
   );
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,67 +15,69 @@ import elrond from '../../assets/elrond.png';
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="top">
-        <span className="logo">
-          <img src={elrond} alt="" className="logoImage" />
-        </span>
-        <span className="grey">&nbsp; | Wallet</span>
-      </div>
-      <hr />
-      <div className="center">
-        <ul>
-          <p className="title">Menu</p>
-          <li className="active">
-            <FaChartPie className="icon" />
-            <span>Dashboard</span>
-          </li>
-          <li>
-            <RiMenu4Fill className="icon" />
-            <span>Transactions</span>
-          </li>
-          <li>
-            <RiUploadLine className="icon" />
-            <span>Send</span>
-          </li>
-          <li>
-            <RiDownloadLine className="icon" />
-            <span>Receive</span>
-          </li>
-          <li>
-            <ApprovalOutlinedIcon className="icon" />
-            <span>Sign Message</span>
-          </li>
-          <li>
-            <CgShapeRhombus className="icon" />
-            <span>Tokens</span>
-          </li>
-          <li>
-            <MdOutlineColorLens className="icon" />
-            <span>NFTs</span>
-          </li>
-          <li>
-            <BsArrowLeftRight className="icon" />
-            <span>Buy</span>
-          </li>
-          <p className="title">Staking</p>
-          <li>
-            <FiLock className="icon" />
-            <span>Stake</span>
-          </li>
-          <li>
-            <FiLock className="icon" />
-            <span>Delegate(Legacy)</span>
-          </li>
-          <li>
-            <RiDatabase2Line className="icon" />
-            <span>Validate</span>
-          </li>
-          <p className="title">Issue</p>
-          <li>
-            <ImStack className="icon" />
-            <span>Tokens</span>
-          </li>
-        </ul>
+      <div className="sidebarContainer">
+        <div className="top">
+          <span className="logo">
+            <img src={elrond} alt="" className="logoImage" />
+          </span>
+          <span className="grey">&nbsp; | Wallet</span>
+        </div>
+        <hr />
+        <div className="center">
+          <ul>
+            <p className="title">Menu</p>
+            <li className="active">
+              <FaChartPie className="icon" />
+              <span>Dashboard</span>
+            </li>
+            <li>
+              <RiMenu4Fill className="icon" />
+              <span>Transactions</span>
+            </li>
+            <li>
+              <RiUploadLine className="icon" />
+              <span>Send</span>
+            </li>
+            <li>
+              <RiDownloadLine className="icon" />
+              <span>Receive</span>
+            </li>
+            <li>
+              <ApprovalOutlinedIcon className="icon" />
+              <span>Sign Message</span>
+            </li>
+            <li>
+              <CgShapeRhombus className="icon" />
+              <span>Tokens</span>
+            </li>
+            <li>
+              <MdOutlineColorLens className="icon" />
+              <span>NFTs</span>
+            </li>
+            <li>
+              <BsArrowLeftRight className="icon" />
+              <span>Buy</span>
+            </li>
+            <p className="title">Staking</p>
+            <li>
+              <FiLock className="icon" />
+              <span>Stake</span>
+            </li>
+            <li>
+              <FiLock className="icon" />
+              <span>Delegate(Legacy)</span>
+            </li>
+            <li>
+              <RiDatabase2Line className="icon" />
+              <span>Validate</span>
+            </li>
+            <p className="title">Issue</p>
+            <li>
+              <ImStack className="icon" />
+              <span>Tokens</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
